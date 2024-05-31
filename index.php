b4aa13efe8bc3f8bd0dc1a8fce5d9609c8942c97
Emm-Anuel100 <chinweokwuemmanuel2004@gmail.com>
2024-05-31 12:44:01 +0100

    footer on index page

--- index.php
+++ index.php
@@ -12,8 +12,9 @@
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/brands.min.css" integrity="sha512-8RxmFOVaKQe/xtg6lbscU9DU0IRhURWEuiI0tXevv+lXbAHfkpamD4VKFQRto9WgfOJDwOZ74c/s9Yesv3VvIQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-      <!-- style link -->
+      <!-- external styling -->
       <link rel="stylesheet" href="./styles/dist/cart.css">
+      <link href="./styles/dist/admin_index.css" rel="stylesheet">
    </head>
 <body>
 
@@ -43,7 +44,13 @@
          </span>
       </a>
      </section>
+	 
+	    <br/><br/><br/>
+         <footer class="footer">
+           Dadral Stores <span>&copy;2012 - <?= Date('Y') ?></span>
+         </footer>
 </section>
+
 
 <noscript>Pls you need to enable javascript in your browser!</noscript>
 </body>
@@ -51,7 +58,10 @@
 <!-- Internal styles -->
 <style type="text/css">
    *{
-      transition: ease-in .9s;
+      transition: none;
+   }
+   section.main-content{
+	   overflow: visible !important;
    }
    section.activity{
     position: relative;
@@ -79,11 +89,25 @@
    }
    section.activity a:hover{
       background: linear-gradient(80deg, #dd5f24, #e64124);
-      transition: ease-in .3s;
    }
    section.activity a:hover span:is(.admin,.cashier){
-      bottom: 2px;
+      bottom: 6px;
       position: relative;
+	  transition: ease-in 9s !important;
+   }
+   footer.footer{
+	   padding-top: 12px;
+	   border-top: 1px dashed #eee;
+	   text-align: center;
+	   position: absolute;
+	   top: auto;
+	   transform: translate(55%,26vh);
+	   color: #e74022;
+	   font-size: 14px;
+	   
+	   span{
+		   font-size: 12px;
+	   }
    }
 </style>
 </html>
